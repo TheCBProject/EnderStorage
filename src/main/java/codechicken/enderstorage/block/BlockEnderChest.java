b5899724d1d819152db137e50987a9787bc4ef66
covers1624 <laughlan.cov@internode.on.net>
2021-01-21 13:16:44 +1030

    Use new CCL VoxelShape stuff and bump versions

--- src/main/java/codechicken/enderstorage/block/BlockEnderChest.java
+++ src/main/java/codechicken/enderstorage/block/BlockEnderChest.java
@@ -4,32 +4,29 @@ import codechicken.enderstorage.misc.EnderKnobSlot;
 import codechicken.enderstorage.tile.TileEnderChest;
 import codechicken.enderstorage.tile.TileFrequencyOwner;
 import codechicken.lib.math.MathHelper;
-import codechicken.lib.raytracer.IndexedCuboid6;
-import codechicken.lib.raytracer.SubHitVoxelShape;
+import codechicken.lib.raytracer.IndexedVoxelShape;
+import codechicken.lib.raytracer.MultiIndexedVoxelShape;
+import codechicken.lib.raytracer.VoxelShapeCache;
 import codechicken.lib.vec.*;
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by covers1624 on 29/10/19.
  */
 public class BlockEnderChest extends BlockEnderStorage {
 
-    private static final IndexedCuboid6 CHEST = new IndexedCuboid6(0, new Cuboid6(1 / 16D, 0, 1 / 16D, 15 / 16D, 14 / 16D, 15 / 16D));
-    private static final IndexedCuboid6[][] BUTTONS = new IndexedCuboid6[4][3];
-    private static final IndexedCuboid6[] LATCH = new IndexedCuboid6[4];
+    private static final IndexedVoxelShape CHEST = new IndexedVoxelShape(VoxelShapeCache.getShape(new Cuboid6(1 / 16D, 0, 1 / 16D, 15 / 16D, 14 / 16D, 15 / 16D)), 0);
+    private static final IndexedVoxelShape[][] BUTTONS = new IndexedVoxelShape[4][3];
+    private static final IndexedVoxelShape[] LATCH = new IndexedVoxelShape[4];
 
-    private static final VoxelShape CHEST_SHAPE = VoxelShapes.create(CHEST.aabb());
     private static final VoxelShape[][] SHAPES = new VoxelShape[4][2];
 
     public static final Transformation[] buttonT = new Transformation[3];
@@ -45,19 +42,19 @@ public class BlockEnderChest extends BlockEnderStorage {
                 cuboid.apply(new Translation(0.5, 0, 0.5));
                 cuboid.apply(buttonT[button]);
                 cuboid.apply(new Rotation((-90 * (rot)) * MathHelper.torad, Vector3.Y_POS).at(new Vector3(0.5, 0, 0.5)));
-                BUTTONS[rot][button] = new IndexedCuboid6(button + 1, cuboid);
+                BUTTONS[rot][button] = new IndexedVoxelShape(VoxelShapeCache.getShape(cuboid), button + 1);
             }
-            LATCH[rot] = new IndexedCuboid6(4, new Cuboid6(new EnderKnobSlot(rot).getSelectionBB()));
+            LATCH[rot] = new IndexedVoxelShape(VoxelShapeCache.getShape(new Cuboid6(new EnderKnobSlot(rot).getSelectionBB())), 4);
 
             //Build all VoxelShapes.
             for (int state = 0; state < 2; state++) {
-                List<IndexedCuboid6> cuboids = new ArrayList<>();
+                ImmutableSet.Builder<IndexedVoxelShape> cuboids = ImmutableSet.builder();
                 cuboids.add(CHEST);
                 if (state == 0) {
-                    Collections.addAll(cuboids, BUTTONS[rot]);
+                    cuboids.add(BUTTONS[rot]);
                     cuboids.add(LATCH[rot]);
                 }
-                SHAPES[rot][state] = new SubHitVoxelShape(CHEST_SHAPE, cuboids);
+                SHAPES[rot][state] = new MultiIndexedVoxelShape(CHEST, cuboids.build());
             }
         }
     }
@@ -68,7 +65,7 @@ public class BlockEnderChest extends BlockEnderStorage {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        VoxelShape shape = CHEST_SHAPE;
+        VoxelShape shape = CHEST;
         TileEntity t = worldIn.getTileEntity(pos);
         if (t instanceof TileEnderChest) {
             TileEnderChest tile = (TileEnderChest) t;
