3d9e4f9940c38f2e6951a1b7faf028f7564547b4
covers1624 <laughlan.cov@internode.on.net>
2022-08-25 20:06:12 +0930

    Add pickaxe minable tags.

--- src/main/java/codechicken/enderstorage/init/DataGenerators.java
+++ src/main/java/codechicken/enderstorage/init/DataGenerators.java
@@ -2,7 +2,8 @@ package codechicken.enderstorage.init;
 
 import codechicken.lib.datagen.ItemModelProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
@@ -12,10 +13,12 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
 
 import static codechicken.enderstorage.EnderStorage.MOD_ID;
+import static codechicken.enderstorage.init.EnderStorageModContent.*;
 
 /**
  * Created by covers1624 on 4/25/20.
@@ -31,6 +34,9 @@ public class DataGenerators {
             gen.addProvider(new BlockStates(gen, files));
             gen.addProvider(new ItemModels(gen, files));
         }
+        if (event.includeServer()) {
+            gen.addProvider(new BlockTagGen(gen, files));
+        }
     }
 
     private static class ItemModels extends ItemModelProvider {
@@ -41,9 +47,9 @@ public class DataGenerators {
 
         @Override
         protected void registerModels() {
-            generated(EnderStorageModContent.ENDER_CHEST_ITEM).noTexture();
-            generated(EnderStorageModContent.ENDER_TANK_ITEM).noTexture();
-            generated(EnderStorageModContent.ENDER_POUCH).noTexture();
+            generated(ENDER_CHEST_ITEM).noTexture();
+            generated(ENDER_TANK_ITEM).noTexture();
+            generated(ENDER_POUCH).noTexture();
         }
 
         @Override
@@ -69,18 +75,22 @@ public class DataGenerators {
             ModelFile model = models()
                     .withExistingParent("dummy", "block")
                     .texture("particle", "minecraft:block/obsidian");
-            simpleBlock(EnderStorageModContent.ENDER_CHEST_BLOCK.get(), model);
-            simpleBlock(EnderStorageModContent.ENDER_TANK_BLOCK.get(), model);
+            simpleBlock(ENDER_CHEST_BLOCK.get(), model);
+            simpleBlock(ENDER_TANK_BLOCK.get(), model);
+        }
+    }
 
-            ModelFile[] soybean_models = new ModelFile[6];
-            for (int i = 0; i < soybean_models.length; i++) {
-                soybean_models[i] = models().cross("soybean_" + i, modLoc("block/soybean/state" + i));
-            }
+    private static class BlockTagGen extends BlockTagsProvider {
 
-            getVariantBuilder(Blocks.WHEAT).forAllStates(state -> ConfiguredModel.builder()
-                    .modelFile(soybean_models[state.getValue(CropBlock.AGE)])
-                    .build()
-            );
+        public BlockTagGen(DataGenerator gen, @Nullable ExistingFileHelper existingFileHelper) {
+            super(gen, MOD_ID, existingFileHelper);
+        }
+
+        @Override
+        protected void addTags() {
+            tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                    .add(ENDER_CHEST_BLOCK.get())
+                    .add(ENDER_TANK_BLOCK.get());
         }
     }
 }
